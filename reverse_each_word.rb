36f5e7931d60d672f5b6622a6cbb648f642478f4
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 15:09:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,24 +1,24 @@
-# DONE WITHOUT COLLECT
-# def reverse_each_word(source_array)
-#   reverse = []
-#   new_array = source_array.split(' ')
-#   final = ""
-#   new_array.each do |val|
-#       reverse.push(val.reverse)
-#   end
-#   final = reverse.join(' ')
-#   final
-# end
-
-
 DONE WITHOUT COLLECT
 def reverse_each_word(source_array)
   reverse = []
   new_array = source_array.split(' ')
   final = ""
-  final = new_array.collect do |val|
+  new_array.each do |val|
       reverse.push(val.reverse)
   end
-  
+  final = reverse.join(' ')
   final
 end
+
+
+# DONE WITHOUT COLLECT
+# def reverse_each_word(source_array)
+#   reverse = []
+#   new_array = source_array.split(' ')
+#   final = ""
+#   final = new_array.collect do |val|
+#       reverse.push(val.reverse)
+#   end
+#
+#   final
+# end
