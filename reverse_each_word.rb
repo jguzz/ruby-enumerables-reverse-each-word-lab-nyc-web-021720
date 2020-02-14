870de0e777148cb55f0bc28f034953393b6806b8
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 14:44:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,7 @@ def reverse_each_word(source_array)
   new_array = source_array.split(' ')
   final = ""
   new_array.each do |val|
-    reverse.push(val.reverse)
-    reverse.each do |v|
       final += val.reverse
-    end
   end
   final
 end
