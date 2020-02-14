1560d4ddf2bf37812fd6000ecc01c0485150ab1e
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 15:02:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,7 +14,7 @@ def reverse_each_word(source_array)
 
   new_array = source_array.split(' ')
   final = ""
-  new_array.collect do |val|
+  new_array.map do |val|
     val.reverse
   end
   new_array
